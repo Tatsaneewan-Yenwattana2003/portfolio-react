9438dd85f08959b9fc3bd28febe6bf9ad66b2a6f
tatsaneewan yenwattana <153794678+Tatsaneewan-Yenwattana2003@users.noreply.github.com>
2024-03-05 22:45:44 +0700

    qq

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,28 +7,28 @@ function Portfolio() {
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
-            <img src="1.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-digital-buggu-374559.jpg" alt="" />
+            <p>Project Income-expense money slip system by ocr</p>
         </div>
         <div className={styles.port_items}>
-            <img src="2.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-lee-campbell-89724.jpg" alt="" />
+            <p>Project wab app </p>
         </div>
         <div className={styles.port_items}>
-            <img src="3.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-luis-gomes-546819.jpg" alt="" />
+            <p>Project Application Scan sea creatures</p>
         </div>
         <div className={styles.port_items}>
-            <img src="4.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-mizuno-k-12899188.jpg" alt="" />
+            <p>Project Electrical circuit</p>
         </div>
         <div className={styles.port_items}>
-            <img src="5.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-pixabay-270404.jpg" alt="" />
+            <p>Project Game</p>
         </div>
         <div className={styles.port_items}>
-            <img src="6.jpg" alt="" />
-            <p>Project Title</p>
+            <img src="src/components/Portfolio/pexels-rodrigo-santos-3888149.jpg" alt="" />
+            <p>Project Basic number system</p>
         </div>
       </div>
     </div>
