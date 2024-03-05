408d38fc320130b3aee0c5d37c07a6c7bc85b56d
tatsaneewan yenwattana <153794678+Tatsaneewan-Yenwattana2003@users.noreply.github.com>
2024-03-05 23:00:51 +0700

    g

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -42,7 +42,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="src/components/Portfolio/309695153_458480182921010_7544557642122114074_n.jpg"
+            src="https://images.unsplash.com/photo-1543852786-1cf6624b9987?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
           />
           <h4>Watermelon</h4>
