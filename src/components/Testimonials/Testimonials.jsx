52a42e511b12771aa353167d036728c7a7baa0e2
tatsaneewan yenwattana <153794678+Tatsaneewan-Yenwattana2003@users.noreply.github.com>
2024-03-05 22:56:02 +0700

    e

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -42,7 +42,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="src/components/Portfolio/309695153_458480182921010_7544557642122114074_n.jpg"
             alt=""
           />
           <h4>Watermelon</h4>
